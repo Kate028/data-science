2c70d0960b6e65a0ae98310969bd7ed2651c4038
Kritika Ranjan <kritikaranjan28@gmail.com>
2022-04-03 21:40:36 +0530

    Update Salaries Differences.sql

--- SQL/StrataScratch/Salaries Differences.sql
+++ SQL/StrataScratch/Salaries Differences.sql
@@ -1,4 +1,5 @@
-Salaries Differences
+# Salaries Differences
+
 Write a query that calculates the difference between the highest salaries found in the marketing and engineering departments. Output just the absolute difference in salaries.
 
 Tables: db_employee, db_dept
