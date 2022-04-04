86d45ffe12f1fb68c95fe3246f8b12c9456f78f1
Kritika Ranjan <kritikaranjan28@gmail.com>
2022-04-04 13:16:43 +0530

    Update Popularity of Hack.sql

--- SQL/StrataScratch/Popularity of Hack.sql
+++ SQL/StrataScratch/Popularity of Hack.sql
@@ -1,6 +1,9 @@
 Popularity of Hack
 
-Meta/Facebook has developed a new programing language called Hack.To measure the popularity of Hack they ran a survey with their employees. The survey included data on previous programing familiarity as well as the number of years of experience, age, gender and most importantly satisfaction with Hack. Due to an error location data was not collected, but your supervisor demands a report showing average popularity of Hack by office location. Luckily the user IDs of employees completing the surveys were stored.
+Meta/Facebook has developed a new programing language called Hack.To measure the popularity of Hack they ran a survey with their employees. 
+The survey included data on previous programing familiarity as well as the number of years of experience, age, gender and most importantly 
+satisfaction with Hack. Due to an error location data was not collected, but your supervisor demands a report showing average popularity 
+of Hack by office location. Luckily the user IDs of employees completing the surveys were stored.
 Based on the above, find the average popularity of the Hack per office location.
 Output the location along with the average popularity.
 
@@ -36,4 +39,4 @@ employee_id	age	gender	popularity
 select avg(h.popularity) as avg_popularity, e.location
 from facebook_employees e inner join facebook_hack_survey h
 on e.id = h.employee_id
-group by location;
+group by e.location;
